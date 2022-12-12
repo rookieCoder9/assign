10d0b799fd93ff2ce66033762d88ab0271b3dc68
Sahil Kumar <sahil_kumar.ug20@nsut.ac.in>
2022-12-12 21:47:01 +0530

    Add useRef usage example

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useRef } from "react";
+import "./App.css";
 
 function App() {
+  const myref = useRef();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 ref={myref}>Hello</h1>
+      <button onClick={() => (myref.current.innerText = "Hello World!")}>
+        Click Me !
+      </button>
     </div>
   );
 }
